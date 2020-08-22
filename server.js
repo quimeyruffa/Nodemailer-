c824e3fc5b11b949260662103c66af6f6cde5034
Quimey <quimeyzoe@hotmail.com>
2020-08-22 00:55:45 -0300

    Now we are using express!

--- server.js
+++ server.js
@@ -1,40 +1,57 @@
 require('dotenv').config();
-const express = require("express");
-const nodemailer= require("nodemailer")
+
+const path = require('path');
+const express = require('express');
+const nodemailer= require("nodemailer");
+const bodyParser = require('body-parser')
+
+
 const app = express();
-const path = require("path")
+const port = 8000;
+const viewDirectory = path.join(__dirname, '../views/');
 
-const PORT = 8080;
+app.use(express.static(viewDirectory));
+app.use(bodyParser.json());
 
-
+app.post('/send_email', (req, res) => {
+    console.log('we are in');
+    var email = req.body.email;
+    var text = req.body.text;
+    console.log(email);
 
 // Step 1 -> What going to connect to whichever host domain 
 //  that using or either services that you'd like to connect you
-let transpoter = nodemailer.createTransport({
-    //  i'd like to connect to Gmail so i do service and make it 
-    // equal to Gmail
-     service: 'gmail',
-     auth:{
-        //  configure this one so that it is not visible to the entire public
-         user:process.env.email,
-         pass:process.env.password
-     }
-})
+    let transporter = nodemailer.createTransport({
+        //  i'd like to connect to Gmail so i do service and make it 
+        // equal to Gmail
+        service: 'gmail',
+        auth:{
+            //  configure this one so that it is not visible to the entire public
+            user:process.env.email,
+            pass:process.env.password
+        }
+    })
+    let mailOptions={
+        from: 'quimeyzoe86@gmail.com',
+        to: email,
+        subject:'Testing and testing',
+        text: text
+    }
 
+  
 // Step 2 -> what are the things that i'd like to send to my mail 
-let mailOptions={
-    from: 'quimeyzoe86@gmail.com',
-    to:'quimeyzoe86@gmail.com',
-    subject:'Testing and testing',
-    text:'It works!'
-}
+
 
 // Step 3 -> Grap whatever you have the transporter on step number one
-transpoter.sendMail(mailOptions, function(err, data){
+transporter.sendMail(mailOptions, function(err, data){
     if (err){
         // We need to disable that Gmail feacture https://myaccount.google.com/lesssecureapps
         console.log('Errors Occurs', err)
     } else{
-        console.log('Email sent!!')
+        console.log('Email sent!!' + data.response)
     }
 })
+});
+app.listen(port, () => {
+    console.log('Running on ' + port);
+});
